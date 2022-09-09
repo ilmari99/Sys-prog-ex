9f2a910a60315db5719f97f0cbd1f1e3fbe059be
ilmari99 <i.vahteristo@gmail.com>
2022-09-09 13:21:04 +0300

    T3

--- W2/W2T3.py
+++ W2/W2T3.py
@@ -1,5 +1,14 @@
 #https://stackoverflow.com/questions/3996026/what-is-the-default-encoding-for-c-strings
+
+import sys
 from ctypes import *
 libc = CDLL("libc.dll.6")
-libc.printf("hello everybody\n".encode('ascii'))    # Tämä pitää muuttaa ascii:ksi, koska C vaatii pääsääntöisesti ascii yhteensopivat stringit
+# Tämä pitää muuttaa ascii:ksi, koska Pythonin C kirjastot vaativat pääsääntöisesti ascii yhteensopivat stringit
+libc.printf("hello everybody\n".encode('ascii'))
 
+# JOS tekstiä ei muuta ascii:ksi, saa OSError 8, joka on format error.
+
+# Tämä printtaa Pythonin oman moduulin kautta, kun taas edellä printataan 'suoraan' C funktiolla
+def printf(format, *args):
+    sys.stdout.write(format % args)
+    return
