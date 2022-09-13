a43acb7abf19f519e533a0a62a41fc7c6f33cbce
ilmari99 <i.vahteristo@gmail.com>
2022-09-13 11:56:09 +0300

    T2 ja T3, koska vain yksi tiedosto voi palauttaa

--- W2/W2T3.py
+++ W2/W2T3.py
@@ -1,3 +1,21 @@
+# TEHTÄVÄV 2
+# Kutsumalla ohjelmaa, käyttöjärjestelmä aloittaa uuden prosessin (virtuaalinen prosessori, muisti).
+# Käytetään käyttöjärjestelmän tarjoamaa compileria ajamaan Pythonin ohjeiksi kääntämä koodi
+# Prosessi käynnistää Terminaalin tapaisen taustalla. Python avaa terminaalin ikkunassa.
+
+import math # Käytetään 'math' standardikirjastoa, joka on C kielinen moduuli floating-point matikalle
+
+print("Let's calculate some square roots!")     # Ulostetaan texti käyttöjärjestelmän terminaaliin
+
+n = input("Gibe number: ")                      # Otetaan sisään textiä terminaalista, joka käyttää mm. ajuria näppäimistölle
+
+print(math.sqrt(float(n)) if n.isnumeric() else "That's not a number!" )
+
+
+
+###################################################################################################
+# TEHTÄVÄ 3
+
 #https://stackoverflow.com/questions/3996026/what-is-the-default-encoding-for-c-strings
 
 import sys
